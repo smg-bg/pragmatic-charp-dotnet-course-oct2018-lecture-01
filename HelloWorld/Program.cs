c27e0fb946ccd586d5f4de394ae01570ac99d5a4
Georgi Panayotov <smg@smg-bg.net>
2018-12-03 18:00:04 +0200

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // start here
+
+            /*  
+             Multiline comment
+             */
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
